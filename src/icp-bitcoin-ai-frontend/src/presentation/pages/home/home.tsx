b6a999ebdb8c23714fefdffdd1a986f0ac977b65
felipesaadi <felipesaadisiegert@gmail.com>
2024-06-05 00:00:31 -0300

    feat: add network infos

--- src/icp-bitcoin-ai-frontend/src/presentation/pages/home/home.tsx
+++ src/icp-bitcoin-ai-frontend/src/presentation/pages/home/home.tsx
@@ -2,14 +2,26 @@ import React, { useState } from 'react'
 import styles from './home-styles.module.scss'
 import Sidebar from '../../components/sidebar/sidebar'
 import Hashblocks from '../../components/hashblocks/hashblocks'
+import Network, { NetworkData } from '../../components/network/network'
 
 const Home: React.FC = () => {
   const [actual, setActual] = useState('Bitcoin')
+  const [data, setData] = useState<NetworkData>(
+    {
+      description: "Bitcoin is the first decentralized cryptocurrency. Nodes in the peer-to-peer bitcoin network verify transactions through cryptography and record them in a public distributed ledger, called a blockchain, without central",
+      transactionsCount: '2020749',
+      transactionsValue: '2980937292746 BTC',
+      address: '12300289033',
+      token: 'BTC USD'
+    }
+  )
+
   return (
     <div className={styles.home}>
       <Sidebar actual={actual} onChange={(coin) => setActual(coin)} />
       <div className={styles.container}>
         <Hashblocks coin={actual} />
+        <Network data={data} />
       </div>
     </div>
   )
