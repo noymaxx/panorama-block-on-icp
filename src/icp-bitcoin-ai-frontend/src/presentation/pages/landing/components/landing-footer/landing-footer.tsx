d8f5389c35e8f09cf7c2624a053dd7240dfd0577
felipesaadi <felipesaadisiegert@gmail.com>
2024-06-13 03:38:10 -0300

    feat: add landing page

---
+++ src/icp-bitcoin-ai-frontend/src/presentation/pages/landing/components/landing-footer/landing-footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './landing-footer-styles.module.scss'
+
+const LandingFooter: React.FC = () => {
+  return (
+    <div className={styles.landingFooter}>
+      <h2 className={styles.title}>Panorama Block</h2>
+
+      <p className={styles.copyright}>
+        Copyright © 2024 - Panorama Block
+      </p>
+    </div>
+  )
+}
+
+export default LandingFooter
