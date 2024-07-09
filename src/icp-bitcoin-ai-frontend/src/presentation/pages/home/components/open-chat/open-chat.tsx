fc020931d24f3b3ac289160f29edc46ec2bd1407
felipesaadi <felipesaadisiegert@gmail.com>
2024-07-09 15:56:33 -0300

    feat: add integrated openchat

--- src/icp-bitcoin-ai-frontend/src/presentation/pages/home/components/open-chat/open-chat.tsx
+++ src/icp-bitcoin-ai-frontend/src/presentation/pages/home/components/open-chat/open-chat.tsx
@@ -2,24 +2,27 @@ import React, { useEffect, useRef, useState } from 'react'
 import styles from './open-chat-styles.module.scss'
 import { initialise } from "@open-ic/openchat-xframe"
 import { OpenChatXFrame } from "@open-ic/openchat-xframe/lib/types"
+import { Close } from '@mui/icons-material'
 
-const purple = "rgb(182, 95, 247)"
-const txt = "#ffffff"
-const txtLight = "#efefef"
-const background = "#242424"
+const purple = "#582CFF75"
+const txt = "#f5f5f5"
+const txtSecondary = "#A0AEC0"
+const background = "#060722"
+const entryBg = "#582CFF10"
+const inputBg = "#0F123B"
 
-function initialiseOpenChatFrame(
+const initialiseOpenChatFrame = (
   path: string,
   iframe: HTMLIFrameElement
-): Promise<OpenChatXFrame> {
+): Promise<OpenChatXFrame> => {
   return initialise(iframe, {
     targetOrigin: "https://oc.app",
-    initialPath: '/community/rfeib-riaaa-aaaar-ar3oq-cai/channel/334961401678552956581044255076222828441',
+    initialPath: path,
     settings: {
       disableLeftNav: true
     },
     theme: {
-      name: "vaultbet",
+      name: "Panorama Block",
       base: "dark",
       overrides: {
         burst: false,
@@ -27,8 +30,8 @@ function initialiseOpenChatFrame(
         bd: purple,
         bg: background,
         txt: txt,
-        placeholder: txtLight,
-        "txt-light": txtLight,
+        placeholder: txtSecondary,
+        "txt-light": txtSecondary,
         timeline: {
           txt: txt,
         },
@@ -48,13 +51,13 @@ function initialiseOpenChatFrame(
           hv: purple,
         },
         icon: {
-          txt: txtLight,
+          txt: txtSecondary,
         },
         currentChat: {
           date: {
             bd: `solid 1px ${purple}`,
             bg: "rgba(0,0,0,0.8)",
-            txt: txtLight,
+            txt: txtSecondary,
           },
           msg: {
             bd: `solid 1px ${purple}`,
@@ -65,20 +68,24 @@ function initialiseOpenChatFrame(
             txt: txt,
           },
         },
+        entry: {
+          bg: entryBg,
+          input: {
+            bg: inputBg,
+          }
+        },
       },
     },
   })
 }
 
 type Props = {
-  path: string
-  title: string
+  onClose: () => void
 }
 
-const OpenChat: React.FC = () => {
+const OpenChat: React.FC<Props> = ({ onClose }: Props) => {
   const iframe = useRef<HTMLIFrameElement>(null)
-  const path = "/community/dgegb-daaaa-aaaar-arlhq-cai/channel/12148470416168947889486180374669069959"
-  const title = "Announcements"
+  const path = "/group/lejtn-6aaaa-aaaar-bijya-cai/?ref=kv2af-gaaaa-aaaaf-bl4cq-cai"
 
   const [client, setClient] = useState<Promise<OpenChatXFrame> | undefined>(
     undefined
@@ -95,6 +102,9 @@ const OpenChat: React.FC = () => {
   return (
     <div className={styles.openChat}>
       <iframe ref={iframe} title="OpenChat" frameBorder="0" />
+      <div className={styles.close} onClick={onClose}>
+        <Close />
+      </div>
     </div>
   )
 }
